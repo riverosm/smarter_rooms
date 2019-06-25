5540219866baefb71507ad85000174ddda14413c
Matias Riveros <riverosm@ar.ibm.com>
2019-06-25 15:44:16 -0300

    bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 90 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with:   VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 8 }
+  has_secure_password
+
 end
