3e6238e6686fd367e438ed1541619b47294329c0
Matias Riveros <riverosm@ar.ibm.com>
2019-07-04 10:17:06 -0300

    Added basic session handler

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      # MR - Change later to rooms_url
+      redirect_to root_url
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      redirect_to login_url
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   resources :users
-  root 'static_pages#home'
 
+  root 'static_pages#home'
   get '/about', to: 'static_pages#about'
+
+  get '/signup', to: 'users#signup'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
