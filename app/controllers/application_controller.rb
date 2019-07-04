88dee125534ca499297b879a708aab4fe68de277
Matias Riveros <riverosm@ar.ibm.com>
2019-07-04 14:29:56 -0300

    Protected users model from normal users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :logged_in_user
   skip_before_action :logged_in_user, only: [:new, :create, :home, :about]
+
+  def verify_if_admin_and_redirect_with_error_message_if_not
+    unless current_user.is_admin?
+      flash[:danger] = 'You are not authorized to perform that action'
+      redirect_to root_url
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  before_action :verify_if_admin_and_redirect_with_error_message_if_not, only: [:index, :destroy]
+
   # GET /users
   # GET /users.json
   def index
