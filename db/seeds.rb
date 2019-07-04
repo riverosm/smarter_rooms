189b89640d372609f943e115647223ad9028ae6a
Matias Riveros <riverosm@ar.ibm.com>
2019-07-04 14:52:48 -0300

    DB seed for admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({name: 'Admin user', email: 'room_reservation@ar.ibm.com', phone: '5286-1111',
+  password: 'CodeAcademy$1', password_confirmation: 'CodeAcademy$1', admin: true})
