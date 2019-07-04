0af82a8b0957678bf74ef1b71b79a2042caab7de
Matias Riveros <riverosm@ar.ibm.com>
2019-07-04 17:21:57 -0300

    Removed comments on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 User.create({name: 'Admin user', email: 'room_reservation@ar.ibm.com', phone: '5286-1111',
   password: 'CodeAcademy$1', password_confirmation: 'CodeAcademy$1', admin: true})
 
