ecda3e1f468aa6849ed695adde7cc1eb16050532
Matias Riveros <riverosm@ar.ibm.com>
2019-07-04 22:02:15 -0300

    Validations on bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
+  before_action :set_room, only: [:new, :create]
+
   def index
   end
 
@@ -9,5 +11,16 @@ class BookingsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+  def set_room
+    @room = Room.where("active = 1 AND id = ?",params[:room_id])
+    if @room.nil?
+      flash[:danger] = "You are not allowed to access this room."
+      redirect_to rooms_url
+    end
+
+    @room_booked = current_user.bookings.active.any?{|br| br.room == @room}
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,19 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validate :not_currently_booked
+  validates :valid_to, presence: true
+  validates :valid_from, presence: true
+
+  scope :active, -> { where("? BETWEEN valid_from AND valid_to", Time.now) }
+  scope :not_active, -> { where("? NOT BETWEEN valid_from AND valid_to", Time.now) }
+
+  def not_currently_booked
+    errors.add(:rental, "You already have an active rental for this Movie") if currently_rented?
+  end
+
+  def currently_booked?
+    Rental.active.where(user: user, room: room).count > 0
+  end
 end
