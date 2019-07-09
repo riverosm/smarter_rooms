61fa93ec1e171861d4a60406b720bece7e1c3294
Matias Riveros <riverosm@ar.ibm.com>
2019-07-09 15:01:31 -0300

    Handling RecordNotFound exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < ActionController::Base
   before_action :logged_in_user
   skip_before_action :logged_in_user, only: [:new, :create, :home, :about]
 
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+
+  def record_not_found
+    flash[:danger] = 'The record was not found.'
+    redirect_to root_url
+  end
+
   def verify_if_admin_and_redirect_with_error_message_if_not
     unless current_user.is_admin?
       flash[:danger] = 'You are not authorized to perform that action'
