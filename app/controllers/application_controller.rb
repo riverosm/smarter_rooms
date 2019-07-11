195157248a7a2a1d47662b82a15f219b701a41c1
Matias Riveros <riverosm@ar.ibm.com>
2019-07-11 11:39:00 -0300

    Application flash.now bugs solved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,13 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   def record_not_found
-    flash.now[:danger] = 'The record was not found.'
+    flash[:danger] = 'The record was not found.'
     redirect_to root_url
   end
 
   def verify_if_admin_and_redirect_with_error_message_if_not
     unless current_user.is_admin?
-      flash.now[:danger] = 'You are not authorized to perform that action'
+      flash[:danger] = 'You are not authorized to perform that action'
       redirect_to root_url
     end
   end
