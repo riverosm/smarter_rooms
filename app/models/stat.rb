33b01408a5604540e14333d50ed63de6237ca1c7
Matias Riveros <riverosm@ar.ibm.com>
2019-07-12 12:01:33 -0300

    Added stats tests for avg and busy rooms

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -6,4 +6,10 @@ class Stat < ApplicationController
   def get_five_rooms_with_more_bookings_from_last_30_days
     Booking.last_30_days.group(:room).order(:count).limit(5).count.map { |u| [u[0].name, u[1]] }.sort_by { |k| -k[1] }
   end
+
+  def stats_tests
+    Booking.group(:room_id, 'strftime("%d/%m/%Y", valid_from)').count
+
+    Booking.group(:room_id).sum('strftime("%H:%m", valid_to) - strftime("%H:%m", valid_from)')
+  end
 end
