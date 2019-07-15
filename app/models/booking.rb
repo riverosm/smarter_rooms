bc45723d42e428ece2dd0c2e6709bf69c06f81af
Matias Riveros <riverosm@ar.ibm.com>
2019-07-15 15:42:35 -0300

    Added validations to Bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -13,12 +13,32 @@ class Booking < ApplicationRecord
   validate :valid_from_greater_valid_to
   validate :exceed_capacity
   validate :not_in_the_past
+  validate :same_day
+  validate :not_in_weekend
+  validate :min_from_time
+  validate :max_to_time
 
   scope :last_30_days, -> {where('created_at > ?', 30.days.ago)}
   scope :booked_now, ->{where("? BETWEEN valid_from AND valid_to", DateTime.now)}
   scope :not_booked_now, ->{where("? NOT BETWEEN valid_from AND valid_to", DateTime.now)}
 
   private
+    def same_day
+      errors.add(:booking, "You can't book in two different days") if valid_to.to_date != valid_from.to_date
+    end
+
+    def min_from_time
+      errors.add(:booking, "The from time is wrong") if valid_from.hour < Rails.configuration.smarter_rooms_calendar_start_time
+    end
+
+    def max_to_time
+      errors.add(:booking, "The to time is wrong") if valid_to.hour > Rails.configuration.smarter_rooms_calendar_end_time
+    end
+
+    def not_in_weekend
+      errors.add(:booking, "You can't book on weekends") if valid_to.on_weekend? || valid_from.on_weekend?
+    end
+
     def valid_from_greater_valid_to
       errors.add(:booking, "To time must be greater than the from time.") if valid_to <= valid_from
     end
@@ -37,8 +57,6 @@ class Booking < ApplicationRecord
       room.bookings.where(valid_from: valid_from + 1.second..valid_to - 1.second).count > 0 || 
       room.bookings.where(valid_to: valid_from + 1.second..valid_to - 1.second).count > 0
     end
-
-
 
     def exceed_capacity
       errors.add(:booking, "You can't exceed the room capacity.") if number_of_attendants > room.max_capacity
--- config/initializers/config_smarter_rooms.rb
+++ config/initializers/config_smarter_rooms.rb
@@ -1,7 +1,7 @@
 SmarterRooms::Application.config.smarter_rooms_rooms_api_url = 'https://ca-3-api.mybluemix.net/'
 SmarterRooms::Application.config.smarter_rooms_rooms_api_path = 'api/v1/rooms/'
 SmarterRooms::Application.config.smarter_rooms_rooms_per_page = 6
-SmarterRooms::Application.config.smarter_rooms_calendar_start_time = '09:00'
-SmarterRooms::Application.config.smarter_rooms_calendar_end_time = '18:00'
+SmarterRooms::Application.config.smarter_rooms_calendar_start_time = 9
+SmarterRooms::Application.config.smarter_rooms_calendar_end_time = 18
 SmarterRooms::Application.config.smarter_rooms_calendar_slot_duration = '00:15'
 SmarterRooms::Application.config.smarter_rooms_calendar_hidden_days = '[0, 6]'
