1f46a716276aeb4f768befa5c5dff0e1bd7d164e
Matias Riveros <riverosm@ar.ibm.com>
2019-07-15 10:49:12 -0300

    Added configuration of num of pages, start time, end time, slot duration

---
+++ config/initializers/config_smarter_rooms.rb
@@ -0,0 +1,6 @@
+SmarterRooms::Application.config.smarter_rooms_rooms_api_url = 'https://ca-3-api.mybluemix.net/'
+SmarterRooms::Application.config.smarter_rooms_rooms_api_path = 'api/v1/rooms/'
+SmarterRooms::Application.config.smarter_rooms_rooms_per_page = 6
+SmarterRooms::Application.config.smarter_rooms_start_time = '09:00'
+SmarterRooms::Application.config.smarter_rooms_end_time = '18:00'
+SmarterRooms::Application.config.smarter_rooms_slot_duration = '00:15'
--- config/initializers/smarter_rooms.rb
+++
@@ -1,2 +0,0 @@
-SmarterRooms::Application.config.smarter_rooms_rooms_api_url = 'https://ca-3-api.mybluemix.net/'
-SmarterRooms::Application.config.smarter_rooms_rooms_api_path = 'api/v1/rooms/'
