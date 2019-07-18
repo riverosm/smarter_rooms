0a41da711ea439d315ce57068f160091280509c2
Matias Riveros <riverosm@ar.ibm.com>
2019-07-18 12:44:45 -0300

    Changed colors of top_five view

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,5 +1,5 @@
 Chartkick.options = {
   height: "400px",
-  colors: ["#1C71C1", "#B0E0E6", "#0000CD", "#4169E1", "#B0C4DE"]
+  colors: ["#1C71C1", "#00BFFF", "#0000CD", "#4169E1", "#00008B"]
   # View https://www.w3schools.com/colors/colors_groups.asp
 }
