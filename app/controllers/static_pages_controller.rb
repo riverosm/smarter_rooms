67f4c1deeff78ed0e25fca7ba3dd548aa29bdb90
Matias Riveros <riverosm@ar.ibm.com>
2019-07-19 13:11:55 -0300

    Added home page for admin and splitted home into user and admin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,21 @@ class StaticPagesController < ApplicationController
   def home
     if !logged_in?
       redirect_to login_path
+    elsif current_user.is_admin?
+      @cant_admin = User.is_admin.count
+      @cant_users = User.is_user.count
+      @total_users = @cant_admin + @cant_users
+
+      @cant_buildings = Building.count
+      @rooms_per_building = Room.active.joins(:building).group("buildings.name").count
+
+      @cant_rooms_active = Room.active.count
+      @cant_rooms_inactive = Room.inactive.count
+      @total_rooms = @cant_rooms_active + @cant_rooms_inactive
+
+      @cant_bookings_past = Booking.where("valid_from < ?", DateTime.now).count
+      @cant_bookings_next = Booking.where("valid_from > ?", DateTime.now).count
+      @total_bookings = @cant_bookings_past + @cant_bookings_next
     end
   end
 
