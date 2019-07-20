de723925c30c5a1224aa306acfe2868834b83c51
Matias Riveros <riverosm@ar.ibm.com>
2019-07-20 12:40:15 -0300

    User home restyling

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -17,6 +17,13 @@ class StaticPagesController < ApplicationController
       @cant_bookings_past = Booking.where("valid_from < ?", DateTime.now).count
       @cant_bookings_next = Booking.where("valid_from > ?", DateTime.now).count
       @total_bookings = @cant_bookings_past + @cant_bookings_next
+    else
+      max_results = 5
+
+      @bookings_today = current_user.bookings.where(valid_from: DateTime.now..DateTime.now.end_of_day).order("valid_from, valid_to").limit(max_results)
+      @bookings_lasts = current_user.bookings.where("valid_from < ?", DateTime.now).order("valid_from DESC, valid_to DESC").limit(max_results)
+      @bookings_nexts = current_user.bookings.where("valid_from > ?", DateTime.tomorrow.beginning_of_day).order("valid_from, valid_to").limit(max_results)
+      @most_booked_rooms = current_user.rooms.group("rooms.id").order("count_all DESC").limit(max_results).count
     end
   end
 
