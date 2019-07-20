25b5ac3cfb1ea96cc3a83bb1540e0a96aba8dd0c
Matias Riveros <riverosm@ar.ibm.com>
2019-07-20 16:14:14 -0300

    Stats colors changed

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,5 +1,5 @@
 Chartkick.options = {
   height: "400px",
-  colors: ["#1C71C1", "#00BFFF", "#0000CD", "#4169E1", "#00008B"]
+  colors: ["#1C71C1", "#00BFFF", "#0000FF", "#ADD8E6", "#00008B"]
   # View https://www.w3schools.com/colors/colors_groups.asp
 }
